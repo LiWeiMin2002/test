dbe56f0c76bf38ec5fafce527f2b7e6c5dd29cdf
lwm <1060395480@qq.com>
2023-08-20 15:16:23 +0800

    第一次提交

---
+++ src/main/java/com/yc/Hello.java
@@ -0,0 +1,15 @@
+package com.yc;
+
+/**
+ * @BelongsProject: test
+ * @BelongsPackage: com.yc
+ * @Author: liweimin
+ * @CreateTime: 2023-08-20  15:15
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
